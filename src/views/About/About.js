c3103e31772ce8798601d45057461d8bb418648d
Poonam Digole <poonamdigole123@gmail.com>
2023-10-02 22:52:02 +0530

    added about poster

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -1,13 +1,36 @@
 import "./About.css"
-import img from "./vegetables-img"
+import Navbar from "./../../components/Navbar/Navbar";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import AboutCard from "../../components/Cards/AboutCard";
 
-function About(){
-    return(
+
+function About() {
+    return (
         <>
-              <div>
-               <img src={img}/>
-              </div>
+            <div className="about-container">
+                <Navbar />
+                <h1 className="main-heading">ABOUT</h1>
+            </div>
+
+            <div className="container text-center">
+                <div className="row m-5">
+
+                    <div className="col-6">
+                        <div>
+                        {/* <AboutCard name= "Poonam Digole" education = "Btech Final Year (IT)"/> */}
+                        </div>
+                    </div>
+
+                    <div className="col-6">
+                        {/* Column */}
+                    </div>
+
+                </div>
+            </div>
+
+
         </>
     )
 }
- export default About;
+export default About;
